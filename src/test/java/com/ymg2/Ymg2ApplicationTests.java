c86b24c8b2b686f2f2c4be485ae1835fe656ec14
İrem Şimşek <bmirror59@gmail.com>
2025-05-23 03:56:02 +0300

    ilk

---
+++ src/test/java/com/ymg2/Ymg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ymg2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
